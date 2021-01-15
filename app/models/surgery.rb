2d13f7dbac4f8b440b0f99b0c701922bc5df7755
ninesky00 <jiangzho@gmail.com>
2021-01-15 12:41:52 -0600

    add surgeries on the same day method, add method to view

--- app/models/surgery.rb
+++ app/models/surgery.rb
@@ -1,9 +1,13 @@
 class Surgery < ApplicationRecord
   has_many :surgery_doctors
   has_many :doctors, through: :surgery_doctors
-  enum day: %i(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+  enum day: %i(monday tuesday wednesday thursday friday)
 
   def doctor_names
     doctors.pluck(:name)
+  end
+
+  def self.surgeries_on_same_day(day)
+    select("title").where(day: "#{day}").pluck(:title)
   end
 end
