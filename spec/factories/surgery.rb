4dcac2834f1084f0db4cbc54a5b1b4e44fa5cc14
ninesky00 <jiangzho@gmail.com>
2021-01-15 12:41:13 -0600

    add feature test show other surgeries

--- spec/factories/surgery.rb
+++ spec/factories/surgery.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :surgery, class: Surgery do
     title { Faker::Food.dish }
-    day { %i(Monday Tuesday Wednesday Thursday Friday).sample }
+    day { %i(monday tuesday wednesday thursday friday).sample }
     room_number { Faker::Number.within(range: 1..100) }
   end
 end
--- spec/features/surgery/show_spec.rb
+++ spec/features/surgery/show_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Surgery" do 
   before :each do 
-    @surgery = FactoryBot.create(:surgery)
+    @surgery = FactoryBot.create(:surgery, day: :tuesday)
+    @other_surgeries = FactoryBot.create_list(:surgery, 3, day: :tuesday)
+    @not_on_same_day = FactoryBot.create_list(:surgery, 3, day: :friday)
   end
   describe "show page" do 
     it "has a link from the surgery index page" do 
@@ -19,5 +21,18 @@ RSpec.describe "Surgery" do
       expect(page).to have_content(@surgery.room_number)
     end
 
+    it "displays all surgeries happening on the same day of the week" do 
+      visit surgery_path(@surgery)
+
+      within("#other-surgeries") do 
+        @other_surgeries.each do |surgery|
+          expect(page).to have_content(surgery.title)
+        end
+
+        @not_on_same_day.each do |surgery|
+          expect(page).to_not have_content(surgery.title)
+        end
+      end
+    end
   end
 end
